7524e1b3059c7ac920f586b56a35dedb45886827
ryanef <ryanef39@gmail.com>
2024-03-05 13:25:12 -0500

    Lbs (#6)
    * first vpc
    
    * change prod
    
    * provider
    
    * provider, region
    
    * lbs'

--- locals.tf
+++ locals.tf
@@ -5,7 +5,11 @@ locals {
       aws_region = "us-east-1"
     }
 
+<<<<<<< HEAD
     TFE_CONTROLLER_PROD = {
+=======
+    TF_VPC_PROD = {
+>>>>>>> 44a3b7c (change prod)
       environment = "prod"
       aws_region = "us-east-1"
     }
